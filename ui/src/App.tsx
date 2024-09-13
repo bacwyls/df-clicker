5f1d164ea40fe47dc2acf9e025aa964928183f57
bacwyls <bacwyls@gmail.com>
2024-09-13 13:02:06 -0500

    puddle gh

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,11 +1,27 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "@dartfrog/puddle";
+import { BrowserRouter as Router, Route, Routes, useParams } from "react-router-dom";
 import { NoServiceView, HalfChat } from '@dartfrog/puddle';
 import { PROCESS_NAME, WEBSOCKET_URL } from "./utils";
 import useClickerStore from "./store/clicker";
 import ClickerPluginBox from "./components/ClickerPluginBox";
 
 function App() {
+
+
+  return (
+    <Router basename={`/${PROCESS_NAME}`}>
+      <Routes>
+        <Route path="/" element={
+          <NoServiceView processName={PROCESS_NAME} websocketUrl={WEBSOCKET_URL} ourNode={window.our?.node} />
+        } />
+        <Route path="/df/service/:id" element={<ServiceRoute />} />
+      </Routes>
+    </Router>
+  );
+}
+
+function ServiceRoute() {
+  const { id } = useParams();
 
   const {handleUpdate} = useClickerStore();
 
@@ -16,23 +32,15 @@ function App() {
   };
 
   return (
-    <Router basename={`/${PROCESS_NAME}`}>
-      <Routes>
-        <Route path="/" element={
-          <NoServiceView processName={PROCESS_NAME} websocketUrl={WEBSOCKET_URL} ourNode={window.our?.node} />
-        } />
-        <Route path="/df/service/:id" element={
-          <HalfChat
-            ourNode={window.our?.node}
-            Element={ClickerPluginBox}
-            processName={PROCESS_NAME}
-            websocketUrl={WEBSOCKET_URL}
-            onServiceMessage={onServiceMessage}
-            enableChatSounds
-           />
-        } />
-      </Routes>
-    </Router>
+    <HalfChat
+      ourNode={window.our?.node}
+      Element={ClickerPluginBox}
+      processName={PROCESS_NAME}
+      websocketUrl={WEBSOCKET_URL}
+      onServiceMessage={onServiceMessage}
+      enableChatSounds
+      paramServiceId={id}
+    />
   );
 }
 
